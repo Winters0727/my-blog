dbaa503a85327b7892176e135ef36f247fe8ca92
winters <winters0727@gmail.com>
2024-03-25 00:49:49 +0900

    refactor: 블로그 이미지 경로 수정

--- src/app/constant/post.ts
+++ src/app/constant/post.ts
@@ -3,4 +3,4 @@ export const POST_COUNT = 5;
 export const DEFAULT_COMMENT_PROFILE_IMAGE =
   "/images/default-comment-profile.png";
 
-export const IMAGE_ENDPOINT = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/images`;
+export const IMAGE_ENDPOINT = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/blog/images`;
